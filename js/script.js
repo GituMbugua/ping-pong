9760a8d1a6c1fc4dbd9cada4707d432552c25c16
Gitu Mbugua <gmbugua38@gmail.com>
2017-06-09 13:07:48 +0300

    edit js script separating business logic and user interface

--- js/script.js
+++ js/script.js
@@ -3,17 +3,22 @@ $(document).ready(function() {
     event.preventDefault();
 
     var number = $("input#userNumber").val();
-    for (var i = 1; i <= number; i++) {
-
-      if (i % 15 === 0) {
-        $('#list').append('<li>' + "Ping-Pong" + '</li>');
-      } else if (i % 3 === 0) {
-        $('#list').append('<li>' + "Ping" + '</li>');
-      } else if (i % 5 === 0) {
-        $('#list').append('<li>' + "Pong" + '</li>');
-      } else {
-        $('#list').append('<li>' + i + '</li>');
-      }
-    }
+    var result = pingPong(number);
+    $("div#result").text(result);
   });
 });
+
+var pingPong = function(number) {
+  for (var i = 1; i <= number; i++) {
+
+    if (i % 15 === 0) {
+      $('#list').append('<li>' + "Ping-Pong" + '</li>');
+    } else if (i % 3 === 0) {
+      $('#list').append('<li>' + "Ping" + '</li>');
+    } else if (i % 5 === 0) {
+      $('#list').append('<li>' + "Pong" + '</li>');
+    } else {
+      $('#list').append('<li>' + i + '</li>');
+    }
+  }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Ping-Pong
+####written by: Gitu Mbugua
+
+  This webpage takes a number from the user and counts up to it. When it gets to a number
+divisible by 3, it replaces that number with "ping"; when it gets to a number 
