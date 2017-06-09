091dfa3e6f5bd9525b303f704837a72604ca2ba2
Gitu Mbugua <gmbugua38@gmail.com>
2017-06-09 18:11:56 +0300

    change js script and html style

--- js/script.js
+++ js/script.js
@@ -1,22 +1,34 @@
+function pingPong(number) {
+  var empty = [];
+
+  for (var i = 1; i <= number; i++) {
+    if (i % 15 === 0) {
+      empty.push('Ping Pong')
+    } else if (i % 3 === 0) {
+      empty.push('Ping');
+    } else if (i % 5 === 0) {
+      empty.push('Pong')
+    } else {
+      empty.push(i);
+    }
+  }
+  console.log(empty);
+  return empty;
+}
+
+
 $(document).ready(function() {
   $("form#submit").submit(function(event) {
     event.preventDefault();
     var number = $("input#userNumber").val();
+    $(".results").text(" ");
     var result = pingPong(number);
-    $("#result").text(result);
+
+    $(".results").append('<ul>');
+
+    result.forEach(function(newNumber) {
+      $(".results").append('<li>' + newNumber + '</li>')
+    })
+    $(".results").append('</ul>')
   });
 });
-
-var pingPong = function(number) {
-  for (var i = 1; i <= number; i++) {
-    if (i % 15 === 0) {
-      $('#list').append('<li>' + "Ping-Pong" + '</li>');
-    } else if (i % 3 === 0) {
-      $('#list').append('<li>' + "Ping" + '</li>');
-    } else if (i % 5 === 0) {
-      $('#list').append('<li>' + "Pong" + '</li>');
-    } else {
-      $('#list').append('<li>' + i + '</li>');
-    }
-  }
-}
