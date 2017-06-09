6f57f461a3a948ecaa6fd65138c9d67d673e412d
Gitu Mbugua <gmbugua38@gmail.com>
2017-06-09 16:26:29 +0300

    add styling and header

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   $("form#submit").submit(function(event) {
     event.preventDefault();
-
     var number = $("input#userNumber").val();
     var result = pingPong(number);
-    $("div#result").text(result);
+    $("#result").text(result);
   });
 });
 
 var pingPong = function(number) {
   for (var i = 1; i <= number; i++) {
-
     if (i % 15 === 0) {
       $('#list').append('<li>' + "Ping-Pong" + '</li>');
     } else if (i % 3 === 0) {
