fd543f0b7cb31a39964396a31b29cf1a266f21c7
Gitu Mbugua <gmbugua38@gmail.com>
2017-06-09 12:52:26 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("form#submit").submit(function(event) {
+    event.preventDefault();
+
+    var number = $("input#userNumber").val();
+    for (var i = 1; i <= number; i++) {
+
+      if (i % 15 === 0) {
+        $('#list').append('<li>' + "Ping-Pong" + '</li>');
+      } else if (i % 3 === 0) {
+        $('#list').append('<li>' + "Ping" + '</li>');
+      } else if (i % 5 === 0) {
+        $('#list').append('<li>' + "Pong" + '</li>');
+      } else {
+        $('#list').append('<li>' + i + '</li>');
+      }
+    }
+  });
+});
